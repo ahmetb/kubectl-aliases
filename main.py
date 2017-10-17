47339c2f7f0c1429015efc8ff1b97896e65bb93c
Ahmet Alp Balkan <ahmetb@google.com>
2017-10-17 12:54:01 -0700

    generator kind of works
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- main.py
+++ main.py
@@ -4,15 +4,15 @@ import itertools
 def main():
     # (alias,full,restrict_to_aliases)
 
-    cmds = [
+    cmds=[
         ('k', 'kubectl', None),
     ]
 
-    globs = [
+    globs=[
         ('s','--namespace=kube-system', None),
     ]
 
-    ops = [
+    ops=[
         ('d','describe', None),
         ('g','get', None),
         ('rm','delete', None),
@@ -20,16 +20,16 @@ def main():
         ('lo','logs -f', None),
     ]
 
-    res = [
-        ('po','pod', ['g','d']),
-        ('dep','deployment', ['g','d']),
+    res=[
+        ('po','pod', ['g','d','lo']),
+        ('dep','deployment', ['g','d','lo']),
         ('svc','service', ['g','d']),
         ('ing','ingress', ['g','d']),
         ('cm','configmap', ['g','d']),
         ('sec','secret', ['g','d']),
     ]
 
-    args = [
+    args=[
         ('oyaml','-o=yaml', ['g']),
         ('owide','-o=wide', ['g']),
         ('all', '--all-namespaces', ['g']),
@@ -42,33 +42,49 @@ def main():
         (cmds, False, True),
         (globs, True, False),
         (ops, False, True),
-        (res, False, True),
+        (res, True, True),
         (args, True, False)
     ]
 
     out = gen(parts)
 
 def gen(parts):
-    out = []
+    out = ['']
     for (items, optional, take_exactly_one) in parts:
         combos = []
-        if take_exactly_one: combos = combo_exactly_n(items, 1)
-        else: combos = combos_all(items, include_0=optional)
-        # todo permutate
+        orig=list(out)
 
-        print '-----', len(combos)
+        if optional and take_exactly_one: combos=combos.append([])
+
+        if take_exactly_one: combos = combinations(items, 1, include_0=optional)
+        else: combos = combinations(items, len(items), include_0=optional)
+
+                # permutate the combinations if optional
+        if optional:
+            new_combos = []
+            for c in combos:
+                new_combos += list(itertools.permutations(c))
+            combos = new_combos
+
+        print '-----'
+
+        new_out=[]
         for c in combos:
-            print c
+            segment = ''.join([s[0] for s in c])
+            print segment if segment else "''"
+            for stuff in orig:
+                new_out.append(stuff+segment)
+            # print [s[0] for s in c] if len(c) else "[]", '-->', orig
+
+        print '\t', '\n\t'.join(new_out)
+        out=new_out
 
 
-def combo_exactly_n(a, n):
-    return [i for i in itertools.combinations(a, n)]
-
-def combos_all(a, include_0=True):
+def combinations(a, n, include_0=True):
     l = []
-    for j in xrange(0, len(a)+1):
+    for j in xrange(0, n+1):
         if not include_0 and j == 0: continue
-        l += itertools.combinations(a, j)
+        l += list(itertools.combinations(a, j))
     return l
 
 if __name__=='__main__':
