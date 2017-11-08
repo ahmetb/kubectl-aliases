9f613cd157cde190896437030ac7b8c035359bf1
Ahmet Alp Balkan <ahmetb@google.com>
2017-11-07 19:37:17 -0800

    Run through pythoniter
    Signed-off-by: Ahmet Alp Balkan <ahmetb@google.com>

--- generate_aliases.py
+++ generate_aliases.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python2
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 # Copyright 2017 Google Inc.
 #
@@ -20,121 +21,131 @@ import os.path
 import sys
 
 try:
-    xrange          # Python 2
+    xrange  # Python 2
 except NameError:
     xrange = range  # Python 3
 
 
 def main():
+
     # (alias, full, allow_when_oneof, incompatible_with)
 
-    cmds=[
-        ('k', 'kubectl', None, None),
-    ]
+    cmds = [('k', 'kubectl', None, None)]
 
-    globs=[
-        ('sys','--namespace=kube-system', None, ['sys']),
-    ]
+    globs = [('sys', '--namespace=kube-system', None, ['sys'])]
 
-    ops=[
-        ('a','apply -f', None, None),
-        ('ex','exec -i -t', None, None),
-        ('lo','logs -f', None, None),
-        ('p','proxy', None, ['sys']),
-        ('g','get', None, None),
-        ('d','describe', None, None),
-        ('rm','delete', None, None),
-        ('run','run --rm -i -t', None, None),
-    ]
+    ops = [
+        ('a', 'apply -f', None, None),
+        ('ex', 'exec -i -t', None, None),
+        ('lo', 'logs -f', None, None),
+        ('p', 'proxy', None, ['sys']),
+        ('g', 'get', None, None),
+        ('d', 'describe', None, None),
+        ('rm', 'delete', None, None),
+        ('run', 'run --rm -i -t', None, None),
+        ]
 
-    res=[
-        ('po','pods', ['g','d','rm'], None),
-        ('dep','deployment', ['g','d','rm'], None),
-        ('svc','service', ['g','d','rm'], None),
-        ('ing','ingress', ['g','d','rm'], None),
-        ('cm','configmap', ['g','d','rm'], None),
-        ('sec','secret', ['g','d','rm'], None),
-        ('no','nodes',['g','d'], ['sys']),
-        ('ns','namespaces',['g','d','rm'], ['sys']),
-    ]
+    res = [
+        ('po', 'pods', ['g', 'd', 'rm'], None),
+        ('dep', 'deployment', ['g', 'd', 'rm'], None),
+        ('svc', 'service', ['g', 'd', 'rm'], None),
+        ('ing', 'ingress', ['g', 'd', 'rm'], None),
+        ('cm', 'configmap', ['g', 'd', 'rm'], None),
+        ('sec', 'secret', ['g', 'd', 'rm'], None),
+        ('no', 'nodes', ['g', 'd'], ['sys']),
+        ('ns', 'namespaces', ['g', 'd', 'rm'], ['sys']),
+        ]
     res_types = [r[0] for r in res]
 
-    args=[
-        ('oyaml','-o=yaml', ['g'], ['owide','ojson','sl']),
-        ('owide','-o=wide', ['g'], ['oyaml','ojson']),
-        ('ojson','-o=json', ['g'], ['owide','oyaml','sl']),
-        ('all', '--all-namespaces', ['g','d'], ['rm', 'f', 'no', 'sys']),
-        ('sl', '--show-labels', ['g'], ['oyaml', 'ojson'] + diff(res_types, ['po','dep'])),
-        ('all', '--all', ['rm'], None), # caution: reusing alias
-        ('w', '--watch', ['g'], ['oyaml','ojson','owide']),
-    ]
+    args = [  # caution: reusing alias
+        ('oyaml', '-o=yaml', ['g'], ['owide', 'ojson', 'sl']),
+        ('owide', '-o=wide', ['g'], ['oyaml', 'ojson']),
+        ('ojson', '-o=json', ['g'], ['owide', 'oyaml', 'sl']),
+        ('all', '--all-namespaces', ['g', 'd'], ['rm', 'f', 'no', 'sys'
+         ]),
+        ('sl', '--show-labels', ['g'], ['oyaml', 'ojson']
+         + diff(res_types, ['po', 'dep'])),
+        ('all', '--all', ['rm'], None),
+        ('w', '--watch', ['g'], ['oyaml', 'ojson', 'owide']),
+        ]
 
     # these accept a value, so they need to be at the end and
     # mutually exclusive within each other.
-    positional_args=[
-        ('f', '-f', ['g', 'd', 'rm'], res_types + ['all', 'l']),
-        ('l', '-l', ['g', 'd', 'rm'], ['f', 'all']),
-        ('n', '--namespace', ['g', 'd', 'rm', 'lo', 'ex'], ['ns', 'no', 'sys', 'all']),
-    ]
+
+    positional_args = [('f', '-f', ['g', 'd', 'rm'], res_types + ['all'
+                       , 'l']), ('l', '-l', ['g', 'd', 'rm'], ['f',
+                       'all']), ('n', '--namespace', ['g', 'd', 'rm',
+                       'lo', 'ex'], ['ns', 'no', 'sys', 'all'])]
 
     # [(part, optional, take_exactly_one)]
-    parts=[
+
+    parts = [
         (cmds, False, True),
         (globs, True, False),
         (ops, True, True),
         (res, True, True),
         (args, True, False),
         (positional_args, True, True),
-    ]
+        ]
 
     out = gen(parts)
     out = filter(is_valid, out)
 
     # prepare output
+
     if not sys.stdout.isatty():
-        header_path=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'license_header')
-        with open(header_path, 'r') as f: print(f.read())
+        header_path = \
+            os.path.join(os.path.dirname(os.path.realpath(__file__)),
+                         'license_header')
+        with open(header_path, 'r') as f:
+            print(f.read())
     for cmd in out:
-        print("alias {}='{}'".format(
-            ''.join([a[0] for a in cmd]),
-            ' '.join([a[1] for a in cmd])
-        ))
+        print("alias {}='{}'".format(''.join([a[0] for a in cmd]),
+              ' '.join([a[1] for a in cmd])))
+
 
 def gen(parts):
     out = [()]
     for (items, optional, take_exactly_one) in parts:
-        orig=list(out)
+        orig = list(out)
         combos = []
 
-        if optional and take_exactly_one: combos=combos.append([])
+        if optional and take_exactly_one:
+            combos = combos.append([])
 
-        if take_exactly_one: combos = combinations(items, 1, include_0=optional)
-        else: combos = combinations(items, len(items), include_0=optional)
+        if take_exactly_one:
+            combos = combinations(items, 1, include_0=optional)
+        else:
+            combos = combinations(items, len(items), include_0=optional)
 
         # permutate the combinations if optional (args are not positional)
+
         if optional:
             new_combos = []
             for c in combos:
                 new_combos += list(itertools.permutations(c))
             combos = new_combos
 
-        new_out=[]
+        new_out = []
         for segment in combos:
             for stuff in orig:
-                new_out.append(stuff+segment)
-        out=new_out
+                new_out.append(stuff + segment)
+        out = new_out
     return out
+
 
 def is_valid(cmd):
     for i in xrange(0, len(cmd)):
+
         # check at least one of requirements are in the cmd
-        requirements=cmd[i][2]
+
+        requirements = cmd[i][2]
         if requirements:
-            found=False
+            found = False
             for r in requirements:
                 for j in xrange(0, i):
                     if cmd[j][0] == r:
-                        found=True
+                        found = True
                         break
                 if found:
                     break
@@ -142,13 +153,14 @@ def is_valid(cmd):
                 return False
 
         # check none of the incompatibilities are in the cmd
-        incompatibilities=cmd[i][3]
+
+        incompatibilities = cmd[i][3]
         if incompatibilities:
-            found=False
+            found = False
             for inc in incompatibilities:
                 for j in xrange(0, i):
                     if cmd[j][0] == inc:
-                        found=True
+                        found = True
                         break
                 if found:
                     break
@@ -157,16 +169,22 @@ def is_valid(cmd):
 
     return True
 
+
 def combinations(a, n, include_0=True):
     l = []
-    for j in xrange(0, n+1):
-        if not include_0 and j == 0: continue
+    for j in xrange(0, n + 1):
+        if not include_0 and j == 0:
+            continue
         l += list(itertools.combinations(a, j))
     return l
 
-def diff(a,b):
-    return list(set(a)-set(b))
 
-if __name__=='__main__':
+def diff(a, b):
+    return list(set(a) - set(b))
+
+
+if __name__ == '__main__':
     main()
 
+
+			
